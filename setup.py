13c4aacf44e5226bea06791f341dbc57d0960bdc
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2020-10-12 17:42:10 +0000

    setup.py cleaner wheel compatibility tag check (1/2)

--- setup.py
+++ setup.py
@@ -21,10 +21,9 @@ finally:
     readme_file.close()
 
 
-def bdist_wheel_args_check(_2_or_3, args=sys.argv):
+def bdist_wheel_args_check(tag, args=sys.argv):
     if 'bdist_wheel' not in args:
         return False
-    tag = 'py' + str(_2_or_3)
     if '--python-tag' in args and tag in args:
         return True
     if ('--python-tag=' + tag) in args:
@@ -41,9 +40,9 @@ else:
     modules = ['raise_']
 
     packaged_path = os.path.join(project_directory, 'raise_.py')
-    if bdist_wheel_args_check(3):
+    if bdist_wheel_args_check('py3'):
         source_path = os.path.join(project_directory, 'raise_3.py')
-    elif bdist_wheel_args_check(2):
+    elif bdist_wheel_args_check('py2'):
         source_path = os.path.join(project_directory, 'raise_2.py')
     elif sys.version_info >= (3,):
         source_path = os.path.join(project_directory, 'raise_3.py')
